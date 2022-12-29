c39684f611718c871de871bd6be34a261b2502c4
Дмитрий <Dmitriy.samylov@yandex.ru>
2022-12-29 07:18:25 +0300

    Dialog Enity

---
+++ AnimatorDialogueEnity.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorDialogueEnity : MonoBehaviour
+{
+    public Animator StartAnim;
+    public ManagerEnity dm;
+    public GameObject TriggerEnity;
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        StartAnim.SetBool("StartOpen", true);
+    }
+
+
+    public void OnTriggerExit2D(Collider2D other)
+    {
+        StartAnim.SetBool("StartOpen", false);
+        dm.EndDialogue();
+        Destroy(TriggerEnity);
+        Enity.speed = 0.020f;
+    }
+}
---
+++ TriggerEnity.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerEnity : MonoBehaviour
+{
+    public Dialogues dialogue;
+
+    public void TriggerDialogue()
+    {
+        FindObjectOfType<ManagerEnity>().StartDialogue(dialogue);
+    }
+}
