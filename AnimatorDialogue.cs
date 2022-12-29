62cb2cc249be794e671bd9e75984f9ff64c29914
Дмитрий <Dmitriy.samylov@yandex.ru>
2022-12-29 07:19:13 +0300

    Dialog main

---
+++ AnimatorDialogue.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorDialogue : MonoBehaviour
+{
+    public Animator StartAnim;
+    public Manager dm;
+    public GameObject Trigger;
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        StartAnim.SetBool("StartOpen", true);
+    }
+
+
+    public void OnTriggerExit2D(Collider2D other)
+    {
+        StartAnim.SetBool("StartOpen", false);
+        dm.EndDialogue();
+        Destroy(Trigger);
+    }
+}
