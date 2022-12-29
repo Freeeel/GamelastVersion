fbb58845819bb4146ab58928ff796a0a151330f8
Дмитрий <Dmitriy.samylov@yandex.ru>
2022-12-29 07:13:52 +0300

    fix script

---
+++ DestroyObject.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyObject : MonoBehaviour
+{
+    public GameObject EnityDial;
+    public GameObject zav;
+    void Update()
+    {
+        if (Trigger1floor.conva == true)
+        {
+            Destroy(EnityDial);
+            Destroy(zav);
+        }
+    }
+}
---
+++ Local.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Local : MonoBehaviour
+{
+    public void Update()
+    {
+        if(Player.Death == true && Pause.PauseSet == true)
+        {
+            Time.timeScale = 0f;
+            Player.speed = 0f;
+        }
+        else if (Player.Death == true || Pause.PauseSet == true)
+        {
+            Time.timeScale = 0f;
+            Player.speed = 0f;
+        }
+        else if (Player.Death == false && Pause.PauseSet == false)
+        {
+            Time.timeScale = 1f;
+            Player.speed = 1f;
+        }
+    }
+}
